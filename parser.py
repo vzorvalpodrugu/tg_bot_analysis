e1ef82d1be6d25595738add0c4044435f9cf6238
vzorvalpodrugu <Starkovqw8904@gmail.com>
2026-02-02 21:12:46 +0300

    Описан класс JsonParser

--- parser.py
+++ parser.py
@@ -5,10 +5,24 @@ class JsonParser:
         self.path = path
 
     def pars(self):
-        with open(self.path, "r", encoding="utf-8") as file:
-            context = file.read()
+        try:
+            with open(self.path, "r", encoding="utf-8") as file:
+                context = json.load(file)
+                context = list(context.items())[0][1] #list
+                # print(type(context))
+        except JsonParser:
+            Exception("Json wrong")
+
+        iterator = iter(context)
+        while True:
+            try:
+                el = next(iterator)
+                print(el)
+            except StopIteration:
+                break
+
         return context
 
 if __name__ == "__main__":
     parser = JsonParser('data/videos.json')
-    print(parser.pars())
+    parser.pars()
